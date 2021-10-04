7232c1d522aafcf00823c80b8945227959ed96f4
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-04 12:32:09 +0200

    Handle ending_one and ending_two

--- src/partition.py
+++ src/partition.py
@@ -27,13 +27,17 @@ class Partition():
     def measure_sequence(self):
         partition = copy.deepcopy(self)
         safety = 1
-        safety_stop = 100000
+        safety_stop = 100_000
         current = 1
         end = self.total_number_of_measures
         sequence = []
+        jumps = {}
         while current <= end and safety < safety_stop:
             safety += 1
             sequence.append(current)
+            print(f"iter = {safety}, sequence = {sequence}")
+            # Find next mesure to add to the sequence
+            # Case 1 : if we reach an unused backward
             next_backward = next(iter(partition.repeat_backward), None)
             if next_backward is not None and next_backward == current:
                 print(f"found backward repeat {next_backward}, sequence = {sequence}")
@@ -41,7 +45,20 @@ class Partition():
                 previous_forward = max([i for i in partition.repeat_forward if i < current])
                 print(f"found forward repeat {previous_forward}, sequence = {sequence}")
                 current = previous_forward
+                ending_one_candidates = [i for i in partition.ending_one
+                                            if i < next_backward and i > previous_forward]
+                if ending_one_candidates:
+                    jump_start = ending_one_candidates[0]
+                    jump_stop = min([i for i in partition.ending_two if i > jump_start])
+                    print(f"found jump inside repeat at {jump_start} going to {jump_stop}")
+                    jumps[jump_start] = jump_stop
                 continue
+
+            # Case 2 : if we reach a memorized jump
+            if current in jumps:
+                current = jumps[current]
+                continue
+            # Case 5 : else simply read next one
             current += 1
 
         return sequence
