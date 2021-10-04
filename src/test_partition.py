17624a72b9a8a4bc151ab9b8ea2e13437502929c
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-04 10:31:52 +0200

    Improved pytest readability

--- src/test_partition.py
+++ src/test_partition.py
@@ -14,6 +14,9 @@ situation_7 = load_situation(7)
 situation_8 = load_situation(8)
 
 
+# TEST measure_sequence()
+# =======================
+
 def test_sequence_situation_0():
     """ With no reapeat symbols at all """
     partition = Partition(total_number_of_measures=5)
@@ -21,7 +24,61 @@ def test_sequence_situation_0():
     answer = [1, 2, 3, 4, 5]
     assert sequence == answer
 
+
 def test_sequence_situation_1():
-    part_1 = Partition.from_situation(situation_1)
-    sequence = part_1.measure_sequence()
+    partition = Partition.from_situation(situation_1)
+    sequence = partition.measure_sequence()
     assert sequence == situation_1['measure_sequence']
+
+
+
+
+# TEST is_valid()
+# ===============
+
+def test_is_valid_situation_1():
+    partition = Partition.from_situation(situation_1)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_1['valid']
+
+
+def test_is_valid_situation_2():
+    partition = Partition.from_situation(situation_2)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_2['valid']
+
+
+def test_is_valid_situation_3():
+    partition = Partition.from_situation(situation_3)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_3['valid']
+
+
+def test_is_valid_situation_4():
+    partition = Partition.from_situation(situation_4)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_4['valid']
+
+
+def test_is_valid_situation_5():
+    partition = Partition.from_situation(situation_5)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_5['valid']
+
+
+def test_is_valid_situation_6():
+    partition = Partition.from_situation(situation_6)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_6['valid']
+
+
+def test_is_valid_situation_7():
+    partition = Partition.from_situation(situation_7)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_7['valid']
+
+
+def test_is_valid_situation_8():
+    partition = Partition.from_situation(situation_8)
+    valid_value = partition.is_valid()
+    assert valid_value == situation_8['valid']
