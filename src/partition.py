40997a62e7a36b87abaea46f55cadd90a619df3f
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-04 13:21:44 +0200

    Refactor

--- src/partition.py
+++ src/partition.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import copy
+import logging
 
 from dataclasses import dataclass
 from dataclasses import field
@@ -23,12 +24,43 @@ class Partition():
         total_number_of_measures = situation['total_number_of_measures']
         return Partition(total_number_of_measures=total_number_of_measures, **lists)
 
+    def first_backward(self):
+        """ Returns the first repeat_backward position or None if nothing is found. """
+        return next(iter(self.repeat_backward), None)
+
+    def first_dalsegno(self):
+        """ Returns the first dalsegno position or None if nothing is found. """
+        return next(iter(self.dalsegno), None)
+
+    def first_dacapo(self):
+        """ Returns the first dacapo position or None if nothing is found. """
+        return next(iter(self.dacapo), None)
+
+    def previous_forward_from(self, position):
+        """ Returns the first repeat_forward position previous to given position """
+        return max([i for i in self.repeat_forward if i < position])
+
+    def previous_segno_from(self, position):
+        """ Returns the first segno position previous to given position """
+        return max([i for i in self.segno if i < position])
+
+    def find_ending_one_between(self, begin, end):
+        """ Returns the list of ending_one position between begin and end """
+        return [i for i in self.ending_one if begin < i and i < end]
+
+    def find_tocoda_between(self, begin, end):
+        """ Returns the list of tocoda position between begin and end """
+        return [i for i in self.tocoda if begin < i and i < end]
+
 
     def measure_sequence(self):
         partition = copy.deepcopy(self)
         safety = 1
         safety_stop = 100_000
         current = 1
+        next_backward = partition.first_backward()
+        next_dalsegno = partition.first_dalsegno()
+        next_dacapo = partition.first_dacapo()
         end = self.total_number_of_measures
         sequence = []
         jumps = {}
@@ -38,21 +70,20 @@ class Partition():
             print(f"iter = {safety}, sequence = {sequence}")
             # Find next mesure to add to the sequence
             # Case 1 : if we reach an unused backward
-            next_backward = next(iter(partition.repeat_backward), None)
             if next_backward is not None and next_backward == current:
-                print(f"found backward repeat {next_backward}, sequence = {sequence}")
-                partition.repeat_backward.remove(next_backward)
-                previous_forward = max([i for i in partition.repeat_forward if i < current])
+                print(f"found backward repeat {current}, sequence = {sequence}")
+                previous_forward = partition.previous_forward_from(current)
                 print(f"found forward repeat {previous_forward}, sequence = {sequence}")
-                current = previous_forward
                 # Find ending_ones to memorize jumps
-                ending_one_candidates = [i for i in partition.ending_one
-                                            if i < next_backward and i > previous_forward]
+                ending_one_candidates = partition.find_ending_one_between(previous_forward, current)
                 if ending_one_candidates:
                     jump_start = ending_one_candidates[0]
                     jump_stop = min([i for i in partition.ending_two if i > jump_start])
                     print(f"found jump inside repeat at {jump_start} going to {jump_stop}")
                     jumps[jump_start] = jump_stop
+                current = previous_forward
+                partition.repeat_backward.remove(next_backward)
+                next_backward = partition.first_backward()
                 continue
 
             # Case 2 : if we reach a memorized jump
@@ -61,37 +92,35 @@ class Partition():
                 continue
 
             # Case 3 : if we reach a dal segno
-            next_dalsegno = next(iter(partition.dalsegno), None)
             if next_dalsegno is not None and next_dalsegno == current:
-                print(f"found dal segno {next_dalsegno}, sequence = {sequence}")
-                partition.dalsegno.remove(next_dalsegno)
+                print(f"found dal segno {current}, sequence = {sequence}")
                 previous_segno = max([i for i in partition.segno if i < current])
                 print(f"found segno {previous_segno}, sequence = {sequence}")
-                current = previous_segno
                 # Find coda to memorize jumps
-                tocoda_candidates = [i for i in partition.tocoda
-                                            if i < next_dalsegno and i > previous_segno]
+                tocoda_candidates = partition.find_tocoda_between(previous_segno, current)
                 if tocoda_candidates:
                     jump_start = tocoda_candidates[0]
                     jump_stop = min([i for i in partition.coda if i > jump_start])
                     print(f"found jump inside dalsegno at {jump_start} going to {jump_stop}")
                     jumps[jump_start] = jump_stop
+                current = previous_segno
+                partition.dalsegno.remove(next_dalsegno)
+                next_dalsegno = partition.first_dalsegno()
                 continue
 
             # Case 4 : if we reah a da capo
-            next_dacapo = next(iter(partition.dacapo), None)
             if next_dacapo is not None and next_dacapo == current:
                 print(f"found da capo {next_dacapo}, sequence = {sequence}")
-                partition.dacapo.remove(next_dacapo)
-                current = 1
                 # Find coda to memorize jumps
-                tocoda_candidates = [i for i in partition.tocoda
-                                            if i < next_dacapo]
+                tocoda_candidates = partition.find_tocoda_between(1, current)
                 if tocoda_candidates:
                     jump_start = tocoda_candidates[0]
                     jump_stop = min([i for i in partition.coda if i > jump_start])
                     print(f"found jump inside dalsegno at {jump_start} going to {jump_stop}")
                     jumps[jump_start] = jump_stop
+                current = 1
+                partition.dacapo.remove(next_dacapo)
+                next_dacapo = partition.first_dacapo()
                 continue
 
             # Case 5 : else simply read next one
